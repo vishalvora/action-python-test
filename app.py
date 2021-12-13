a6f7a7c464de7ecc1006f70eb8bdcbcad1a40315
vishalvora <vishalgvora@gmail.com>
2021-12-13 12:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import requests
 print('this msg is from python')
 print('getting data')
 
-x = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+x = requests.get('https://strapi4-dataline4.herokuapp.com/api/clinics/1')
 print(x.json())
-print('data fetching completed')
+a = {data:{
+  'name':'test from github action",
+  'type':'test'
+}}
 
+y = requests.post('https://strapi4-dataline4.herokuapp.com/api/clinics', data = a)
+print(y)
+
+print('data fetching completed')
