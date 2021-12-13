92dc8d4c302a33b66c4b604f1b6e8a016e6c6a15
vishalvora <vishalgvora@gmail.com>
2021-12-13 19:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ print('getting data')
 x = requests.get('https://strapi4-dataline4.herokuapp.com/api/clinics/1')
 print(x.json())
 a = {'data':{
-  'name':'test from github action',
-  'age':500
+  'name':'test app',
+  'store_id':'500'
 }}
 
 y = requests.post('https://strapi4-dataline4.herokuapp.com/api/patients', json = a)
