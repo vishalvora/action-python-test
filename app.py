e446d413893d31fad6bed70c26c48b947b3b738b
vishalvora <vishalgvora@gmail.com>
2021-12-13 12:32:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 print('this msg is from python')
 print('getting data')
 
-x = requests.get(''https://jsonplaceholder.typicode.com/todos/1')
+x = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 print(x)
 print('data fetching completed')
 
