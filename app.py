fcdd157ac78093f3adb0233baa939934ec2794bf
vishalvora <vishalgvora@gmail.com>
2021-12-13 19:38:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ a = {'data':{
   'store_id':'500'
 }}
 
-y = requests.post('https://strapi4-dataline4.herokuapp.com/api/patients', json = a)
+y = requests.post('https://strapi4-applist.herokuapp.com/api/applists', json = a)
 print(y)
 print(y.json())
 
